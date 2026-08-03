3d5b083baa9c68c3349b0757e4fd69685352f9a3
RiyaSingh245 <riyacs435@gmail.com>
2026-08-04 02:16:52 +0530

    added BCrypt

--- app/src/main/java/ticket/booking/utils/UserServiceUtil.java
+++ app/src/main/java/ticket/booking/utils/UserServiceUtil.java
@@ -1,13 +1,15 @@
 package ticket.booking.utils;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class UserServiceUtil {
 
     public static String hashPassword(String plainPassword) {
-        return "test";
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
     }
 
     public static boolean checkPassword(String plainPassword, String hashedPassword) {
-        return true;
+        return BCrypt.checkpw(plainPassword, hashedPassword);
     }
 
     public static String generateTicketId() {
